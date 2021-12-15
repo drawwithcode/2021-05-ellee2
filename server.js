e7e01376757a76d2a787a64c3cb87491df978eee
ellee2 <90704946+ellee2@users.noreply.github.com>
2021-12-15 09:28:38 +0100

    assignment complete

--- server.js
+++ server.js
@@ -7,3 +7,24 @@ let app = express();
 let port = 3000;
 
 let server = app.listen(port);
+
+
+app.use(express.static('public'));
+
+let serverSocket = require('socket.io');
+
+let io = serverSocket(server);
+
+io.on('connection', newConnection);
+
+function newConnection(newSocket) {
+    console.log("new connection:", newSocket.id);
+
+    newSocket.on("mouse", mouseMessage);
+    function mouseMessage(dataReceived) {
+        console.log(dataReceived);
+    
+        newSocket.broadcast.emit("mouseBroadcast", dataReceived);
+    }
+  }
+  
